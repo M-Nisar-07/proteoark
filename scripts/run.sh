0cda6fbfbe1a8329f84f35a04f286f19ccf75fe3
Nisar <mahammadn143@gmail.com>
2025-01-14 11:49:52 +0530

    changed gunicorn

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,4 +5,4 @@ set -e
 python manage.py collectstatic --noinput
 # python manage.py migrate
 
-gunicorn proteoark.wsgi:application --bind 0.0.0.0:8000
+gunicorn proteoark.wsgi:application --bind 0.0.0.0:9000
